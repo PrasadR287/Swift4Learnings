a39d74171e79a87ee0f88f0c279613d806b55adc
Prasad Rotti <prasad.rotti@espn.com>
2018-07-13 13:48:44 +0530

    Initial Commit

---
+++ Swift4Learnings/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift4Learnings
+//
+//  Created by Rotti, Prasad on 13/07/18.
+//  Copyright © 2018 Prasad. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
